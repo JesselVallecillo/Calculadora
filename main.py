4528d8d62ebfc026903ab9a188469f492683f79d
irismargaritas2008-spec <sanchesiris734@gmail.com>
2026-06-18 22:54:19 -0600

    Iris: Agregue la funcion calcular_area_triangulo

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-# main.py - Calculadora Geométrica en equipo modo COLABORATIVO 
+# main.py - Calculadora Geométrica en equipo 
+import math 
+
+def calcular_area_triangulo(base, altura): 
+    return (base * altura) / 2
+
 print("--- BIENVENIDO A LA CALCULADORA GEOMÉTRICA ---") 
+print(f"Área de un triangulo de base 5 y altura 10: {calcular_area_triangulo(5, 10):.2f}")
